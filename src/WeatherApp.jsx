c4b7faf5680ea0be576e64c0dd260b7935e8245f
Juan Gerez <juangerez98a@gmail.com>
2026-05-13 12:22:33 -0300

    Realizamos una aplicacion de clima usando un api y CSS

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import './WeatherApp.css'
+
+export const WeatherApp = () => {
+
+  const [city, setCity] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
+  
+
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+  const API_KEY = 'ba55d9ef528fb51751e8bbaa5834c398'
+  const difKelvin = 273.15 // para obtener los grados celsious debemos restar los numero de los grados Kelvin
+
+
+  const fetchWeatherData = async () =>{
+    try{
+      const response = await fetch (`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`) 
+      const data = await response.json()
+      console.log(data)
+      setWeatherData(data)
+    } catch(error){
+      console.error ('Ha habido un error: ', error) 
+    }
+  }
+
+  const hadleCityChange = (event) => {
+    setCity(event.target.value)
+  }
+
+  const hadleSumit = (event) =>{
+    event.preventDefault()
+    fetchWeatherData()
+  }
+
+
+  return (
+    <div className = "container">
+
+    <h1>Aplicacion de Clima </h1>
+    <form onSubmit={hadleSumit}>
+      <input
+       type="text"
+       placeholder="Ingresa una cuidad"
+       value={city}
+       onChange={hadleCityChange}
+       />
+      <button type="submit">Buscar</button>
+    </form>
+
+    {weatherData && (
+
+      <div>
+        <h2>{weatherData.name},{weatherData.sys.country}</h2>
+        <p>La temperatura es {Math.floor (weatherData.main.temp - difKelvin)}°C</p>
+        <p>La condicion meteorológica actual es: {weatherData.weather[0].description}</p>
+        <img 
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+        alt={weatherData.weather[0].description}
+        />
+      </div>
+      
+    )}
+
+
+   
+
+    </div>
+  )
+}
